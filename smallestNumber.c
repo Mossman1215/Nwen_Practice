736664a4d4e1faa02f5db90573cd9f47442b98e9
mossman1215 <mossman2142@gmail.com>
2013-02-19 20:21:18 +1300

    starting assignment 1

---
+++ smallestNumber.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+#include<limits.h>
+
+int main (){	
+	int number; /* number of integers to be entered */
+	int value; /* value input by user */
+	int smallest = int_MAX; /* smallest number */
+	int i; /* counter */
+
+	//ASK for numbers 
+	//if number is smaller than intmax then assign to smallest
+	//check if next series of numbers are smaller than smallest
+	//i++
+//printf result
+}
