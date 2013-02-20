d5e16a6c5aa07d96e15d22042c7233f3754a1f37
mossman1215 <mossman2142@gmail.com>
2013-02-20 16:10:55 +1300

    compiled and running successfully needs a bug test

--- smallestNumber.c
+++ smallestNumber.c
@@ -5,19 +5,22 @@
 int main (){	
 	int number = 5; /* number of integers to be entered */
 	int value; /* value input by user */
-	int smallest = int_MAX; /* smallest number */
-	int i; /* counter */
+	int smallest = INT_MAX; /* smallest number */
+	int i=0; /* counter */
 
 	//ASK for numbers 
 	//if number is smaller than intmax then assign to smallest
 	//check if next series of numbers are smaller than smallest
 	//i++
 	while(i<number){
-		scanf("Enter one number",value);
+		printf("Enter one number");
+		scanf("%d",&value);
 		if(value < smallest){
 			smallest = value;		
 		}
+		i++;
 	}
 	printf("%d",smallest);
 //printf result
+	return 0;
 }
