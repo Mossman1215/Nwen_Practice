7eeaddb1dc1e210378f83c6c615d90083208d76e
mossman1215 <mossman2142@gmail.com>
2013-03-07 10:53:48 +1300

    asterix first assignment question 2

---
+++ asterix.c
@@ -0,0 +1,20 @@
+/*
+print this
+*
+**
+***
+****
+*/
+#include <stdio.h>
+
+int main(void){
+int row; /* row counter */
+int col; /* column counter */
+	for(row = 0; row<4;row++){
+			for(col = 0; col<4;col++){
+			printf("*");		
+		}
+	}
+
+}
+
