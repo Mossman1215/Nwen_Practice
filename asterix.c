0ae81e5c7a3c21a44113304e66b7a48d1b714478
mossman1215 <mossman2142@gmail.com>
2013-03-07 13:19:25 +1300

    asterix works now

--- asterix.c
+++ asterix.c
@@ -10,11 +10,14 @@ print this
 int main(void){
 int row; /* row counter */
 int col; /* column counter */
+
 	for(row = 0; row<4;row++){
-			for(col = 0; col<4;col++){
-			printf("*");		
-		}
+			for(col = 0; col<row;col++){
+					printf("*");					
+		}		
+		printf("*\n");	
 	}
+		
 	return 0;
 }
 
