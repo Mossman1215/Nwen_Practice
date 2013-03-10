f9e0cecc3cf07fed8c102bf31b4886596eb65c81
mossman1215 <mossman2142@gmail.com>
2013-03-11 11:58:32 +1300

    added the switch question for assignment 1 which is the priority until complete or next tuesday

---
+++ switch.c
@@ -0,0 +1,44 @@
+/*(6 marks) Using the switch statement, write a program that counts and prints the
+number of each different letter grade user entered. Assume we have only five
+different grades – “A” / “a”, “B” / “b”, “C” / “c”, “D” / “d”, “E” / “e”. You need
+to provide a special character to end the input (user needs to enter this character to
+end the input). You can only use the following variables.*/
+#include <stdio.h>
+
+int main (void){
+int grade;
+/* current grade. You can also use char grade.
+You may use either getchar or scanf to handle the input.
+*/
+int aCount = 0; /* total a grades */
+int bCount = 0; /* total b grades */
+int cCount = 0; /* total c grades */
+int dCount = 0; /* total d grades */
+int eCount = 0; /* total e grades */
+
+//while grade is not 'the exit key'
+//ask for a number
+// switch statement
+
+
+}
+/*
+switch from msdn
+for reference
+*/
+/*
+switch( i ) 
+{
+    case -1:
+        n++;
+        break;
+    case 0 :
+        z++;
+        break;
+    case 1 :
+        p++;
+        break;
+}
+
+*/
+
