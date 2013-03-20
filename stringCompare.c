02d515c38a3ee8e4ec2c9c5529bc8f8d40632140
mossman1215 <mossman2142@gmail.com>
2013-03-21 12:05:54 +1300

    Working on string compare file

--- stringCompare.c
+++ stringCompare.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int stringcmp( const char *s1, const char *s2 ); /* prototype */
+int stringlen( const char *s );
 
 int main( void ){
 	
@@ -13,11 +14,29 @@ int main( void ){
 }
 
 int stringcmp(const char *s1, const char *s2 ){
-	int i,j;
-	for(i=0;i<//how to get length of array in c?
-	return -1;
+	int i,j,length1,length2;
+	//find length of string1
+	//check if strings are equally long	otherwise why compare them
+	if(length1 != length2){
+		return 0;
+	}
+	for(i=0;i<length1;i++){
+				
+	}
+	return 0;//if the arrays are different return 0
+}
+
+int stringlen( const char *s ){
+	int i;
+	int sum = 0;
+	for(i=0;i<80;i++){
+		if(s[i] == 0){//scanf ignores return key! //zero is different from '0'
+			printf("return sum of ");			
+			return sum;
+		}
+		sum++;
+	}
+	return 0;
 }
 
 
-
-}
