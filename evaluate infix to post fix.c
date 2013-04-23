1e2302d729afe6d853f6d527f95160f80c329645
mossman1215 <mossman2142@gmail.com>
2013-04-24 10:19:37 +1200

    finally getting started on nwen lab

--- evaluate infix to post fix.c
+++ evaluate infix to post fix.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
-int main(void){
+void convertToPostfix( char infix[], char postfix[] );
+int isOperator( char c );
+/* precedence function returns -1, 0 and 1, respectively.*/
+int precedence( char operator1, char operator2 );
+void push( StackNodePtr *topPtr, char value );
+char pop( StackNodePtr *topPtr );
+char stackTop( StackNodePtr topPtr );
+int isEmpty( StackNodePtr topPtr );
+void printStack( StackNodePtr topPtr );
+/* custom functions*/
+int evaluatePostfixExpression(char*);
+int evaluateBracketed(int*,char*);
+int readNumber(char);
+char match(char);
+void skipWhiteSpace(int*, char*);
 
+int main(void){
 
 	return 0;
 }
--- infix to postfix.c
+++ infix to postfix.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include "MinUnit.h"
+#include <malloc.h>
 struct stackNode {
-char data;
-struct stackNode *nextPtr;
+	char data;
+	struct stackNode *nextPtr;
 };
 typedef struct stackNode StackNode;
 typedef StackNode *StackNodePtr;
@@ -16,15 +17,25 @@ char pop( StackNodePtr *topPtr );
 char stackTop( StackNodePtr topPtr );
 int isEmpty( StackNodePtr topPtr );
 void printStack( StackNodePtr topPtr );
+/* custom functions*/
+void skipWhiteSpace(int*, char*);
 
 int main(void){
 	//need to read data in via scanf or args
+	char input[256];
+	char output[256];
+	scanf("%s",input);
 	// may consider spilling the data up
 	//send to convert to post fix
+	convertToPostfix(input,output);
 	return 0;
 }
 
 //define the functions here
+/* function that converts to postfix and calls printstack*/
+void convertToPostfix(char infix[],char postfix[]){
+
+}
 /*
 *	get node from head pointer store in temp variable and 
 *	make new node with malloc with head as storage? then assign the new head .next
@@ -44,7 +55,7 @@ void push( StackNodePtr *topPtr, char value ){
 *	return the temporary char variable
 */
 char pop( StackNodePtr *topPtr ){
-
+	return 'c';
 }
 /*
 * printing the stack
@@ -59,22 +70,48 @@ void printStack( StackNodePtr topPtr ){
 *	return the headNodes .data value or if its not assigned return null?
 */
 char stackTop( StackNodePtr topPtr ){
-
+	return 'c';
 }
 /*
 * 	check if a value is an operator which are
-*	/*+-^%
+*	/ * + - ^ % 
 * Return 1 (true) if is operator
 *	otherwise return 0 (false)
 */
-int isOperator( char c ){
 
+int isOperator(char c){
+// do a case statement returning an int based on if its a operator otherwise it returns zero (default)
+	switch (c){
+        case '+':
+        return 1; 
+        case '-':
+        return 1;
+        case '*':
+        return 1;
+        case '/':
+        return 1;
+        case'^':
+        return 1;
+        case '%':
+        return 1;
+        default:
+        return 0;    
+    }
 }
+
 /*
 *	check if operator one is before operator 2 in (bedmas?)
 *	return 1 true
 *	return 0 false
 */
 int precedence( char operator1, char operator2 ){
+	return 0;
+}
 
+void skipWhiteSpace(int* point,char* input){
+	int i = *point;
+	while(input[i] == ' '){
+		i++;
+		print("white space skip: %d\n",i);
+	}
 }
